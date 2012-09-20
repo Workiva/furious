b4fa7bb5ef60cc956850d9252fede06e9ac7ebc9
Robert Kluin <robert.kluin@webfilings.com>
2012-09-19 20:50:05 -0500

    Convert _check_job_function to function that also returns options.
    Convert _check_job_function to a "public" function that also gets and
    returns async options specified with the defaults decorator.  The new
    function performs substantially the same functions.

--- furious/job_utils.py
+++ furious/job_utils.py
@@ -2,18 +2,14 @@
 Functions to help with encoding and decoding job information.
 """
 
+import sys
 
-class BadFunctionPath(Exception):
+
+class BadFunctionPathError(Exception):
     """Invalid function path."""
 
-class InvalidJobTuple(Exception):
-    """Invalid job tuple."""
 
-
-
-
-
-def _check_job_function(function):
+def get_function_path_and_options(function):
     """Validates `function` is a potentially valid path or reference to
     a function and returns the cleansed path to the function.
 
@@ -23,23 +19,35 @@ def _check_job_function(function):
 
     Functions passed by reference must be at the top-level of the containing
     module.
+
+    Returns the function path and options.
     """
+    # Try to pop the options off whatever they passed in.
+    options = getattr(function, '_async_options', None)
+
     if isinstance(function, basestring):
         # This is a function name in str form.
         import re
         if not re.match(r'^[^\d\W]([a-zA-Z._]|((?<!\.)\d))+$', function):
-            raise BadFunctionPath(
+            raise BadFunctionPathError(
                 'Invalid function path, must meet Python\'s identifier '
                 'requirements, passed value was "%s".', function)
-        return function
+        return function, options
 
     if callable(function):
         # Try to figure out the path to the function.
         try:
-            return '.'.join((function.__module__, function.func_name))
+            return ('.'.join((function.__module__, function.func_name)),
+                    options)
         except AttributeError:
             if function.__module__ == '__builtin__':
-                return function.__name__
+                return function.__name__, options
+
+        raise BadFunctionPathError("Unable to determine path to callable.")
+
+    raise BadFunctionPathError("Must provide a function path or reference.")
+
+
 
         raise BadFunctionPath("Unable to determine path to callable.")
 
--- furious/tests/test_job_utils.py
+++ furious/tests/test_job_utils.py
@@ -1,61 +1,121 @@
 import unittest
 
 
-def some_function():
-    pass
 
+class TestGetFunctionPathAndOptions(unittest.TestCase):
+    """Make sure junk paths raise exceptions and function args get remapped
+    to a path.
 
-class TestCheckJobFunction(unittest.TestCase):
-    """Make sure junk paths raise and function args get mapped to a path."""
+    Ensure any specified defaults for functions passed by reference are
+    correctly returned.
+    """
 
     def test_valid_name(self):
         """Ensure check job function accepts good input."""
-        from furious.job_utils import _check_job_function
+        from furious.job_utils import get_function_path_and_options
 
         good_names = ['name', 'good.name', 'gr8.name', 'ok.na_me',
                       'Name', 'Good.Name', 'Gr8.Name', 'ok.Na_me']
         for name in good_names:
-            cleansed = _check_job_function(name)
-            self.assertEqual(name, cleansed)
+            cleansed_path, options = get_function_path_and_options(name)
+            self.assertEqual(name, cleansed_path)
+            self.assertIsNone(options)
 
     def test_bad_function_path(self):
-        """Ensure check job function raises BadFunctionPath on bad path."""
-        from furious.job_utils import _check_job_function
-        from furious.job_utils import BadFunctionPath
+        """Ensure get_function_path_and_options function raises
+        BadFunctionPathError when given a bad path.
+        """
+        from furious.job_utils import BadFunctionPathError
+        from furious.job_utils import get_function_path_and_options
 
         bad_names = ['', '0abc', 'test.0abc', 'test.ab-cd',
                      'bad%ness', '.nogood']
         for bad_name in bad_names:
             self.assertRaises(
-                BadFunctionPath, _check_job_function, bad_name)
+                BadFunctionPathError, get_function_path_and_options, bad_name)
 
     def test_none_as_function_path(self):
-        """Ensure check job function raises BadFunctionPath on missing path."""
-        from furious.job_utils import _check_job_function
-        from furious.job_utils import BadFunctionPath
+        """Ensure get_function_path_and_options raises BadFunctionPathError
+        on missing path.
+        """
+        from furious.job_utils import BadFunctionPathError
+        from furious.job_utils import get_function_path_and_options
 
-        self.assertRaises(BadFunctionPath, _check_job_function, None)
+        self.assertRaises(
+            BadFunctionPathError, get_function_path_and_options, None)
 
     def test_gets_callable_path(self):
         """Ensure check job function returns the path of a callable."""
-        from furious.job_utils import _check_job_function
+        from furious.job_utils import get_function_path_and_options
 
-        path = _check_job_function(some_function)
+        def some_function():
+            """This will appear to be a module-level function."""
+            pass
+
+        path, options = get_function_path_and_options(some_function)
         self.assertEqual('furious.tests.test_job_utils.some_function', path)
+        self.assertIsNone(options)
 
     def test_gets_logging_path(self):
         """Ensure check job function returns the path of logging callable."""
-        from furious.job_utils import _check_job_function
+        from furious.job_utils import get_function_path_and_options
         import logging
 
-        path = _check_job_function(logging.info)
+        path, options = get_function_path_and_options(logging.info)
         self.assertEqual('logging.info', path)
+        self.assertIsNone(options)
 
     def test_gets_builtin_path(self):
         """Ensure check job function returns the path of built-in callable."""
-        from furious.job_utils import _check_job_function
+        from furious.job_utils import get_function_path_and_options
 
-        path = _check_job_function(eval)
+        path, options = get_function_path_and_options(eval)
         self.assertEqual('eval', path)
+        self.assertIsNone(options)
+
+    def test_gets_default_options(self):
+        """Ensure check job function returns options off a callable."""
+        from furious.async import defaults
+        from furious.job_utils import get_function_path_and_options
+
+        default_options = {
+            'test': 'options'
+        }
+
+        @defaults(**default_options)
+        def method_with_options():
+            """This will appear to be a module-level function."""
+            pass
+
+        path, options = get_function_path_and_options(method_with_options)
+        self.assertEqual('furious.tests.test_job_utils.method_with_options',
+                         path)
+        self.assertEqual(default_options, options)
+
+    @unittest.skip('This is just a concept.')
+    def test_gets_default_options_from_path(self):
+        """Ensure check job function returns options from a path object.
+
+        NOTE: This is just a concept of how this would work.
+        """
+        from furious.async import FunctionPath
+        from furious.job_utils import get_function_path_and_options
+
+        default_options = {
+            'test': 'options'
+        }
+
+        function_path = FunctionPath("this.is.a.test.function")
+        function_path.update_options(default_options.copy())
+
+        # OR maybe:
+        # function_path = FunctionPath(
+        #     "this.is.a.test.function", default_options.copy())
+
+        path, options = get_function_path_and_options(function_path)
+        self.assertEqual('this.is.a.test.function', path)
+        self.assertEqual(default_options, options)
+
+
 
 
