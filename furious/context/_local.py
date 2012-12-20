f147e1a08178a6f3e1b1dfdb6e71a3e69c138de8
Robert Kluin <robert.kluin@webfilings.com>
2012-12-20 00:34:44 -0600

    Update local context notes and clarify naming.

--- furious/context/_local.py
+++ furious/context/_local.py
@@ -16,12 +16,14 @@
 
 """
 This module contains the logic related to the actual request local context
-object.  This should not typically be used directly by developers.
+object.
+
+NOTE: This should not typically be used directly by developers.
 
 Usage:
 
     # Grab a refernce to the local context object.
-    local_context = local._get_local_context()
+    local_context = local.get_local_context()
 
     # Do something with the local context.
     local_context.registry.append(context.new())
@@ -32,9 +34,17 @@ import os
 import threading
 
 
-# NOTE: Do not import this directly.  If you MUST use this, access it
-# through _get_local_context.
-_local_context = None
+__all__ = ["get_local_context"]
+
+
+def get_local_context():
+    """Return a reference to the current _local_context.
+
+    NOTE: This function is not for general usage, it is meant for
+    special uses, like unit tests.
+    """
+    _init()
+    return _local_context
 
 
 def _init():
@@ -65,12 +75,7 @@ def _init():
     return _local_context
 
 
-def _get_local_context():
-    """Return a reference to the current _local_context.
-
-    NOTE: This function is not for general usage, it is meant for
-    special uses, like unit tests.
-    """
-    _init()
-    return _local_context
+# NOTE: Do not import this directly.  If you MUST use this, access it
+# through get_local_context.
+_local_context = None
 
