14bb181e48799331bdfe68565df34c8d75249901
Tanner Miller <tanner.miller@webfilings.com>
2013-01-16 01:25:06 -0600

    adding basic grep example

---
+++ example/grep.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import os.path
+
+import webapp2
+
+from furious.async import Async
+
+class GrepHandler(webapp2.RequestHandler):
+    def get(self):
+        query = self.request.get('query')
+        curdir = os.getcwd()
+        build_and_start(query, curdir)
+        self.response.out.write('starting grep for query: %s' % query)
+
+def build_and_start(query, directory):
+        async_task = Async(
+            target=grep, args=[query, directory], callbacks={'success': all_done}
+        )
+        async_task.start()
+
+def grep_file(query, item):
+    return ['%s: %s' % (item, line) for line in open(item) if query in line]
+
+def grep(query, directory):
+    dir_contents = os.listdir(directory)
+    results = []
+    for item in dir_contents:
+        path = os.path.join(directory, item)
+        if os.path.isdir(path):
+            build_and_start(query, path)
+        else:
+            if item.endswith('.py'):
+                results.extend(grep_file(query, path))
+    return results
+
+def all_done():
+    from furious.context import get_current_async
+
+    async = get_current_async()
+
+    for result in async.result:
+        logging.info(result)
+
