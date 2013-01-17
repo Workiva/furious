ba2de1c2e715c13c42b2bb96b5808ce2b6275213
Beau Lyddon <beau.lyddon@webfilings.com>
2013-01-17 00:04:04 -0700

    Add MessageProcessor and insert_messsage_processor to batcher
    Add MessageProcessor Async object to the batcher module. This object will
    insert a task to pull tasks off a pull queue. It will try to insert them
    serially using a memcache marker based off a time frequency formula.
    
    Add insert_messsage_processor function that wraps creating a MessageProcessor
    and inserting it.

--- furious/tests/test_batcher.py
+++ furious/tests/test_batcher.py
@@ -1,7 +1,7 @@
 import json
 import unittest
 
-from mock import patch
+from mock import Mock, patch
 
 
 class MessageTestCase(unittest.TestCase):
@@ -190,3 +190,241 @@ class MessageTestCase(unittest.TestCase):
 
         queue_mock.assert_called_once_with(name='my_queue')
         self.assertTrue(queue_mock.return_value.add.called)
+
+
+class MessageProcessorTestCase(unittest.TestCase):
+
+    @patch('furious.batcher.time')
+    @patch('furious.batcher.memcache')
+    def test_to_task_with_no_name_passed_in(self, memcache, time):
+        """Ensure that if no name is passed into the MessageProcessor that it
+        creates a default unique name when creating the task.
+        """
+        from furious.batcher import MessageProcessor
+
+        memcache.get.return_value = 'current-batch'
+        time.time.return_value = 100
+
+        processor = MessageProcessor('something', queue='test_queue')
+
+        task = processor.to_task()
+
+        self.assertEqual(task.name, 'processor-processor-current-batch-3')
+
+    @patch('furious.batcher.time')
+    @patch('furious.batcher.memcache')
+    def test_to_task_with_frequency_passed_in(self, memcache, time):
+        """Ensure that if a frequency is passed into the MessageProcessor that
+        it uses that frequency when creating the task.
+        """
+        from furious.batcher import MessageProcessor
+
+        memcache.get.return_value = 'current-batch'
+        time.time.return_value = 100
+
+        processor = MessageProcessor('something', queue='test_queue', freq=100)
+
+        task = processor.to_task()
+
+        self.assertEqual(task.name, 'processor-processor-current-batch-1')
+
+    @patch('furious.batcher.time')
+    @patch('furious.batcher.memcache')
+    def test_to_task_with_name_passed_in(self, memcache, time):
+        """Ensure that if a name is passed into the MessageProcessor that it
+        uses that name when creating the task.
+        """
+        from furious.batcher import MessageProcessor
+
+        memcache.get.return_value = 'current-batch'
+        time.time.return_value = 100
+
+        processor = MessageProcessor('something', queue='test_queue',
+                                     task_args={'name': 'test-name'})
+
+        task = processor.to_task()
+
+        self.assertEqual(task.name, 'test-name-processor-current-batch-3')
+
+    @patch('furious.batcher.time')
+    @patch('furious.batcher.memcache')
+    def test_to_task_with_tag_passed_in(self, memcache, time):
+        """Ensure that if a tag is passed into the MessageProcessor that it
+        uses that tag when creating the task.
+        """
+        from furious.batcher import MessageProcessor
+
+        memcache.get.return_value = 'current-batch'
+        time.time.return_value = 100
+
+        processor = MessageProcessor('something', queue='test_queue',
+                                     tag='test-tag')
+
+        task = processor.to_task()
+
+        self.assertEqual(task.name, 'processor-test-tag-current-batch-3')
+
+        memcache.get.assert_called_once_with('agg-batch-test-tag')
+
+    @patch('furious.batcher.time')
+    @patch('furious.batcher.memcache')
+    def test_to_task_with_tag_not_passed_in(self, memcache, time):
+        """Ensure that if a tag is not passed into the MessageProcessor that it
+        uses a default value when creating the task.
+        """
+        from furious.batcher import MessageProcessor
+
+        memcache.get.return_value = 'current-batch'
+        time.time.return_value = 100
+
+        processor = MessageProcessor('something', queue='test_queue')
+
+        task = processor.to_task()
+
+        self.assertEqual(task.name, 'processor-processor-current-batch-3')
+
+        memcache.get.assert_called_once_with('agg-batch-processor')
+
+    @patch('google.appengine.api.taskqueue.Task', autospec=True)
+    @patch('furious.batcher.time')
+    @patch('furious.batcher.memcache')
+    def test_to_task_has_correct_arguments(self, memcache, time, task):
+        """Ensure that if no name is passed into the MessageProcessor that it
+        creates a default unique name when creating the task.
+        """
+        from furious.batcher import MessageProcessor
+
+        memcache.get.return_value = 'current-batch'
+        time.time.return_value = 100
+
+        processor = MessageProcessor('something', queue='test_queue')
+
+        processor.to_task()
+
+        task_args = {
+            'url': '/_ah/queue/async/something',
+            'headers': {},
+            'payload': json.dumps({
+                'queue': 'test_queue',
+                'job': ["something", None, None],
+                'task_args': {
+                    'countdown': 30,
+                    'name': 'processor-processor-current-batch-3'
+                },
+            }),
+            'countdown': 30,
+            'name': 'processor-processor-current-batch-3'
+        }
+
+        task.assert_called_once_with(**task_args)
+
+    @patch('furious.batcher.memcache')
+    def test_curent_batch_key_exists_in_cache(self, cache):
+        """Ensure that if the current batch key exists in cache that it uses it
+        and doesn't update it.
+        """
+        from furious.batcher import MessageProcessor
+
+        cache.get.return_value = 1
+
+        processor = MessageProcessor('something')
+
+        current_batch = processor.current_batch
+
+        cache.get.assert_called_once_with('agg-batch-processor')
+
+        self.assertEqual(current_batch, 1)
+        self.assertFalse(cache.add.called)
+
+    @patch('furious.batcher.memcache')
+    def test_curent_batch_key_doesnt_exist_in_cache(self, cache):
+        """Ensure that if the current batch key doesn't exist in cache that it
+        inserts the default value of 1 into cache and returns it.
+        """
+        from furious.batcher import MessageProcessor
+
+        cache.get.return_value = None
+
+        processor = MessageProcessor('something')
+
+        current_batch = processor.current_batch
+
+        self.assertEqual(current_batch, 1)
+
+        cache.get.assert_called_once_with('agg-batch-processor')
+        cache.add.assert_called_once_with('agg-batch-processor', 1)
+
+
+class InsertMessageProcessorTestCase(unittest.TestCase):
+
+    def test_no_countdown_or_name(self):
+        """Ensure that if no countdown or name are set that they aren't passed
+        in as task_args to the MessageProcessor.
+        """
+        from furious.batcher import insert_messsage_processor
+        from furious.batcher import MessageProcessor
+
+        with patch.object(MessageProcessor, 'start') as start:
+            processor = insert_messsage_processor(
+                'job', None, None, 'queue-name')
+
+            options = {
+                'queue': 'queue-name',
+                'job': ('job', None, None),
+                'task_args': {}
+            }
+            self.assertEqual(processor.get_options(), options)
+            self.assertEqual(processor.tag, 'processor')
+            self.assertEqual(processor.frequency, 30)
+
+        start.assert_called_once_with()
+
+    def test_countdown_passed_in(self):
+        """Ensure that if countdown is set that it's passed as a task_arg to
+        the MessageProcessor.
+        """
+        from furious.batcher import insert_messsage_processor
+        from furious.batcher import MessageProcessor
+
+        with patch.object(MessageProcessor, 'start') as start:
+            processor = insert_messsage_processor(
+                'job', None, None, 'queue-name', countdown=100)
+
+            task_args = {
+                'countdown': 100
+            }
+            self.assertEqual(processor.get_task_args(), task_args)
+
+        start.assert_called_once_with()
+
+    def test_name_passed_in(self):
+        """Ensure that if name is set that it's passed as a task_arg to the
+        MessageProcessor.
+        """
+        from furious.batcher import insert_messsage_processor
+        from furious.batcher import MessageProcessor
+
+        with patch.object(MessageProcessor, 'start') as start:
+            processor = insert_messsage_processor(
+                'job', None, None, 'queue-name', name='name')
+
+            task_args = {
+                'name': 'name'
+            }
+            self.assertEqual(processor.get_task_args(), task_args)
+
+        start.assert_called_once_with()
+
+    def test_tag_passed_in(self):
+        """Ensure that if tag is set that it's set on the MessageProcessor."""
+
+        from furious.batcher import insert_messsage_processor
+        from furious.batcher import MessageProcessor
+
+        with patch.object(MessageProcessor, 'start') as start:
+            processor = insert_messsage_processor(
+                'job', None, None, 'queue-name', tag='tag')
+
+            self.assertEqual(processor.tag, 'tag')
+
+        start.assert_called_once_with()
