7133b113646409162e29f1d756bc0d237cbfce1c
Chris Lo Coco <chris.lococo@webfilings.com>
2013-01-24 12:22:32 -0700

    Changed the import.yaml file to be autodetected and included the furious_helper.

---
+++ furious_helper.py
@@ -0,0 +1,9 @@
+"""
+This file is used to help Furious cope with all of the pathing hijinks in the
+bigsky applciation.
+"""
+
+import manage
+manage.setup_project()
+
+from furious.handlers.webapp import app
