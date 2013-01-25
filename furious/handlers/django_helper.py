8641841e084436634e76aebd4b5ebe1a44aa5d9c
Chris Lo Coco <chris.lococo@webfilings.com>
2013-01-25 11:03:21 -0700

    Made more generic.

---
+++ furious/handlers/django_helper.py
@@ -0,0 +1,8 @@
+"""
+This file is used to help Furious cope with django pathing.
+"""
+
+import manage
+manage.setup_project()
+
+from webapp import app
--- furious_helper.py
+++
@@ -1,9 +0,0 @@
-"""
-This file is used to help Furious cope with all of the pathing hijinks in the
-bigsky applciation.
-"""
-
-import manage
-manage.setup_project()
-
-from furious.handlers.webapp import app
