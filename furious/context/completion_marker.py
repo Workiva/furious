b524b869441b5315125183c0163d4815a7c057a8
John.Lockwood <john.lockwood@webfilings.com>
2013-02-04 11:43:18 -0600

    Add imports for function serializing

---
+++ furious/context/completion_marker.py
@@ -0,0 +1,21 @@
+#
+# Copyright 2013 WebFilings, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import math
+import uuid
+from furious.job_utils import decode_callbacks
+from furious.job_utils import encode_callbacks
+from furious.job_utils import function_path_to_reference
+from furious.job_utils import get_function_path_and_options
