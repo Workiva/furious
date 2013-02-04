2d7ca969ff462922950d9d6c40a1ae6450559e1a
John.Lockwood <john.lockwood@webfilings.com>
2013-02-04 14:11:58 -0600

    Add persistence id handling functions
    A context and each Async that is part of it needs unique ID
    and since a graph will be used to join all of them, the internal
    nodes/vertexes of the graph also need an ID too. The ID of every
    Async will be prefixed with it's parent node ID, to allow it to find
    the parent. leaf_persistence_id_from_group_id combines the parent
    ID with a new id for it's child. leaf_persistence_id_to_group_id returns
    the parent ID from a child node.

--- furious/context/completion_marker.py
+++ furious/context/completion_marker.py
@@ -22,3 +22,42 @@ from furious.job_utils import get_function_path_and_options
 from furious.config import get_configured_persistence_module
 persistence_module = get_configured_persistence_module()
 
+
+class InvalidLeafId(Exception):
+    """
+    This leaf id is invalid, it must be prefixed by a group id, comma
+    separated
+    """
+
+def leaf_persistence_id_from_group_id(group_id, index):
+    """
+    Args:
+        group_id: the id of an internal node(internal vertex)
+        index: a string that uniquely identifies this node among
+        it's sibling nodes.
+    Returns:
+        A string that is a comma separated join of the two args
+    """
+    return ",".join([str(group_id), str(index)])
+
+
+def leaf_persistence_id_to_group_id(persistence_id):
+    """
+    Args:
+        persistence_id: a string of a node's id
+    Returns:
+        A string representing the group id of a node.
+    Raises:
+        InvalidLeafId if the id cannot be split by a comma
+        or if the id is not a string
+    """
+    try:
+        parts = persistence_id.split(',')
+        parts.pop()
+        if not parts:
+            raise InvalidLeafId("Id must be prefixed by a group id"
+                                "separated by a comma")
+        group_id = ",".join(parts)
+        return group_id
+    except AttributeError:
+        raise InvalidLeafId("Id must be a basestring")
--- furious/tests/context/test_completion_marker.py
+++ furious/tests/context/test_completion_marker.py
@@ -14,3 +14,48 @@
 # limitations under the License.
 #
 
+from google.appengine.ext import testbed
+from mock import patch
+import unittest
+import uuid
+
+
+class TestFunctions(unittest.TestCase):
+    @patch('furious.context.completion_marker.InvalidLeafId')
+    @patch('furious.context.completion_marker.leaf_persistence_id_from_group_id')
+    @patch('furious.context.completion_marker.leaf_persistence_id_to_group_id')
+    def test_leaf_id_functions(self, InvalidLeafId,
+                               leaf_persistence_id_from_group_id,
+                               leaf_persistence_id_to_group_id):
+        """
+        Ensure the group id can be retrieved from the id given to an async
+        """
+        group_id = uuid.uuid4().hex
+        index = 2
+        leaf_key = leaf_persistence_id_from_group_id(group_id,index)
+        self.assertIsNotNone(leaf_key)
+        self.assertEqual(len(leaf_key.split(',')),2)
+        reconstituted_group_id = leaf_persistence_id_to_group_id(leaf_key)
+        self.assertEqual(reconstituted_group_id,group_id)
+
+        group_id = "{0},2,4".format(uuid.uuid4().hex)
+        index = 2
+        leaf_key = leaf_persistence_id_from_group_id(group_id,index)
+        self.assertIsNotNone(leaf_key)
+        self.assertGreaterEqual(len(leaf_key.split(',')),2)
+        reconstituted_group_id = leaf_persistence_id_to_group_id(leaf_key)
+        self.assertEqual(reconstituted_group_id,group_id)
+
+        def wrapper():
+            return leaf_persistence_id_to_group_id("2")
+
+        self.assertRaises(InvalidLeafId,wrapper)
+
+        def non_string_wrapper():
+            return leaf_persistence_id_to_group_id(2)
+
+        self.assertRaises(InvalidLeafId,non_string_wrapper)
+
+        group_id = leaf_persistence_id_to_group_id(u"234,4")
+        self.assertEqual(u"234",group_id)
+
