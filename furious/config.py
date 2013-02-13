c96f83cc7513e78f91cffe66876cc70e71548986
John.Lockwood <john.lockwood@webfilings.com>
2013-02-13 11:01:12 -0600

    Refactor and document based on code review

--- furious/config.py
+++ furious/config.py
@@ -15,61 +15,97 @@
 #
 import logging
 import os
-import yaml
 from StringIO import StringIO
 
-FURIOUS_YAML_NAMES = ['furious.yaml','furious.yml']
+import yaml
+
+
+FURIOUS_YAML_NAMES = ['furious.yaml', 'furious.yml']
 
 PERSISTENCE_MODULES = {
     'ndb': 'furious.extras.appengine.ndb_persistence'
 }
 
+
 class BadModulePathError(Exception):
     """Invalid module path."""
+
 
 class InvalidPersistenceModuleName(Exception):
     """
     There is no persistence strategy by that name
     """
 
+
 class InvalidYamlFile(Exception):
     """
     The furious.yaml file is invalid yaml
     """
+
 
 class EmptyYamlFile(Exception):
     """
     The furious.yaml file is empty
     """
 
-def module_import(name):
-    try:
-        mod = __import__(name)
-        components = name.split('.')
-        for comp in components[1:]:
-            mod = getattr(mod,comp)
-        return mod
-    except ImportError:
-        raise BadModulePathError(
-            'Unable to find module "%s".' % (name,))
-
-def get_persistence_module(name, valid_modules=PERSISTENCE_MODULES):
-    module_path = valid_modules.get(name)
-    module = None
-    if not module_path:
-        raise InvalidPersistenceModuleName(
-            'no persistence strategy by the name %s'%(name,))
-
-    module = module_import(name=module_path)
-    return module
-
-def get_configured_persistence_module(valid_modules=PERSISTENCE_MODULES):
-    return get_persistence_module(config['persistence'],valid_modules)
 
 class MissingYamlFile(Exception):
     """
     furious.yaml cannot be found
     """
+
+
+def module_import(module_path):
+    """Imports the module indicated in name
+
+    Args:
+        module_path: string representing a module path such as
+        'furious.config' or 'furious.extras.appengine.ndb_persistence'
+    Returns:
+        the module matching name of the last component, ie: for
+        'furious.extras.appengine.ndb_persistence' it returns a
+        reference to ndb_persistence
+    Raises:
+        BadModulePathError if the module is not found
+    """
+    try:
+        # Import whole module path.
+        module = __import__(module_path)
+        # Split into components: ['furious',
+        # 'extras','appengine','ndb_persistence'].
+        components = module_path.split('.')
+        # Starting at the second component, set module to a
+        # a reference to that component. at the end
+        # module with be the last component. In this case:
+        # ndb_persistence
+        for component in components[1:]:
+            module = getattr(module, component)
+        return module
+    except ImportError:
+        raise BadModulePathError(
+            'Unable to find module "%s".' % (module_path,))
+
+
+def get_persistence_module(name, known_modules=PERSISTENCE_MODULES):
+    """Get a known persistence module or one where name is a module path
+    Args:
+        name: name of persistence module
+        known_modules: dictionary of module names and module paths,
+            ie: {'ndb':'furious.extras.appengine.ndb_persistence'}
+    Returns:
+        module of the module path matching the name in known_modules
+        or the module path that is name
+    """
+    module_path = known_modules.get(name) or name
+    module = module_import(module_path=module_path)
+    return module
+
+
+def get_default_persistence_engine(known_modules=PERSISTENCE_MODULES):
+    config = get_config()
+    return get_persistence_module(config['persistence'],
+                                  known_modules=known_modules)
+
 
 def find_furious_yaml(config_file=__file__):
     """
@@ -85,12 +121,13 @@ def find_furious_yaml(config_file=__file__):
         the path of furious.yaml or None if not found
     """
     checked = set()
-    yaml = _find_furious_yaml(os.path.dirname(config_file),checked)
+    yaml = _find_furious_yaml(os.path.dirname(config_file), checked)
     if not yaml:
         yaml = _find_furious_yaml(os.getcwd(), checked)
     return yaml
 
-def _find_furious_yaml(start,checked):
+
+def _find_furious_yaml(start, checked):
     """Traverse the directory tree identified by start
     until a directory already in checked is encountered or the path
     of furious.yaml is found.
@@ -106,24 +143,31 @@ def _find_furious_yaml(start,checked):
     Returns:
         the path of the furious.yaml file or None if it is not found
     """
-    dir = start
-    while dir not in checked:
-        checked.add(dir)
+    directory = start
+    while directory not in checked:
+        checked.add(directory)
         for fs_yaml_name in FURIOUS_YAML_NAMES:
-            yaml_path = os.path.join(dir, fs_yaml_name)
+            yaml_path = os.path.join(directory, fs_yaml_name)
             if os.path.exists(yaml_path):
                 return yaml_path
-        dir = os.path.dirname(dir)
+        directory = os.path.dirname(directory)
     return None
 
+
 def default_config():
+    """The default configuration allows furious to work
+    even without a user furious.yaml
+
+    Returns:
+        dictionary of defaults used by various parts of furious
+    """
     return {'secret_key':
             '931b8-i-f44330b4a5-am-3b9b733f-not-secure-043e96882',
             'persistence': 'ndb',
             'task_system': 'appengine_taskqueue'}
 
 
-def get_config():
+def _parse_yaml_config():
     """
     Gets the configuration from the found furious.yaml
     file and parses the data.
@@ -131,24 +175,29 @@ def get_config():
         a dictionary parsed from the yaml file
     """
     furious_yaml_path = find_furious_yaml()
-    dataMap = default_config()
+    data_map = default_config()
     if furious_yaml_path is None:
-#        raise MissingYamlFile("furious.yaml is missing")
-        logging.warning("furious.yaml is missing, using default config")
-    else:
-        with open(furious_yaml_path) as f:
-    #        TODO: validate the yaml contents
-            #load file contents into a StringIO to enable use of a
-            #a mock file with yaml.load, preventing it from hanging
-            sf = StringIO(f.read())
-            data = yaml.load(sf)
-            if not isinstance(data,dict):
-                if data is None:
-                    raise EmptyYamlFile("The furious.yaml file is empty")
-                else:
-                    raise InvalidYamlFile("The furious.yaml file "
-                                          "is invalid yaml")
-            dataMap.update(data)
-    return dataMap
+    #        raise MissingYamlFile("furious.yaml is missing")
+        logging.debug("furious.yaml is missing, using default config")
+        return data_map
 
-config = get_config()
+    with open(furious_yaml_path) as yaml_file:
+    #        TODO: validate the yaml contents
+        #load file contents into a StringIO to enable use of a
+        #a mock file with yaml.load, preventing it from hanging
+        string_io_file = StringIO(yaml_file.read())
+        data = yaml.load(string_io_file)
+        if not isinstance(data, dict):
+            if data is None:
+                raise EmptyYamlFile("The furious.yaml file is empty")
+            else:
+                raise InvalidYamlFile("The furious.yaml file "
+                                      "is invalid yaml")
+        data_map.update(data)
+    return data_map
+
+
+def get_config():
+    return _config
+
+_config = _parse_yaml_config()
--- furious/tests/test_config.py
+++ furious/tests/test_config.py
@@ -13,27 +13,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import os
+import unittest
+
 from mock import MagicMock
 from mock import Mock
 from mock import patch
-import os
-import re
-import unittest
 
-def splitkeepsep(s, sep):
-    return reduce(lambda acc, i: acc[:-1] + [acc[-1] + i]\
-    if i == sep else acc + [i],
-        re.split("(%s)" % re.escape(sep), s), [])
 
 def mock_get_config_as(yaml_contents):
+    """Mocks open to return yaml_contents instead of furious.yaml
+    Args:
+        yaml_contents: yaml as a string
+    Returns:
+        A config dictionary parsed from the yaml in
+        yaml_contents instead of any actual furious.yaml file.
+    """
     with patch('__builtin__.open', create=True) as mock_open:
         mock_open.return_value = MagicMock(spec=file)
         manager = mock_open.return_value.__enter__.return_value
-        manager.__iter__.return_value = iter(splitkeepsep(
-            yaml_contents, "\n"))
         manager.read.return_value = yaml_contents
-        from ..config import get_config
-        return get_config()
+        from ..config import _parse_yaml_config
+
+        return _parse_yaml_config()
+
 
 class TestConfigurationLoading(unittest.TestCase):
     def test_module_import_missing_module(self):
@@ -41,18 +44,19 @@ class TestConfigurationLoading(unittest.TestCase):
         from ..config import module_import
 
         self.assertRaises(BadModulePathError,
-            module_import,'furious.extras.not_here')
+                          module_import, 'furious.extras.not_here')
 
     def test_module_import(self):
         from ..config import module_import
         from .. import config
 
         module = module_import('furious.config')
-        self.assertEqual(module,config)
+        self.assertEqual(module, config)
 
     def test_not_find_yaml(self):
         os.path.exists = Mock(return_value=False)
         from ..config import find_furious_yaml
+
         config_yaml_path = find_furious_yaml()
         self.assertIsNone(config_yaml_path)
 
@@ -64,10 +68,9 @@ class TestConfigurationLoading(unittest.TestCase):
 
         my_config = mock_get_config_as(example_yaml)
 
-        self.assertEqual(my_config,{'secret_key':'blah',
-                                 'persistence':'bubble',
-                                 'task_system':'flah'})
-
+        self.assertEqual(my_config, {'secret_key': 'blah',
+                                     'persistence': 'bubble',
+                                     'task_system': 'flah'})
 
     def test_get_configured_persistence_exists(self):
         os.path.exists = Mock(return_value=True)
@@ -78,45 +81,59 @@ class TestConfigurationLoading(unittest.TestCase):
         my_config = mock_get_config_as(example_yaml)
 
         from .. import config
-        config.config = my_config
 
-        persistence_module = config.get_configured_persistence_module(
-            {'bubble':'furious.config'})
+        config._config = my_config
 
-        self.assertEqual(persistence_module,config)
+        persistence_module = config.get_default_persistence_engine(
+            {'bubble': 'furious.config'})
 
+        self.assertEqual(persistence_module, config)
 
-    def mock_get_config_raises(self,yaml_contents,exception=Exception):
+    def mock_get_config_raises(self, yaml_contents, exception=Exception):
+        """Mocks open to return yaml_contents instead of furious.yaml
+        and expects yaml_contents to be malformed in a way to
+        cause exception to raise
+        Args:
+            yaml_contents: invalid yaml as a string
+            exception: an exception class
+        """
         with patch('__builtin__.open', create=True) as mock_open:
             mock_open.return_value = MagicMock(spec=file)
             manager = mock_open.return_value.__enter__.return_value
-            manager.__iter__.return_value = iter(splitkeepsep(
-                yaml_contents, "\n"))
             manager.read.return_value = yaml_contents
-            from ..config import get_config
-            self.assertRaises(exception,get_config)
+            from ..config import _parse_yaml_config
+
+            self.assertRaises(exception, _parse_yaml_config)
 
     def test_get_config_invalid_yaml(self):
         from furious.config import InvalidYamlFile
+
         os.path.exists = Mock(return_value=True)
         example_yaml = str('secret_key:"blah"\n'
                            'persistence:bubble\n'
                            'task_system:flah\n')
-        self.mock_get_config_raises(example_yaml,InvalidYamlFile)
+        self.mock_get_config_raises(example_yaml, InvalidYamlFile)
+
+    def test_get_persistence_module(self):
+        from furious.config import get_persistence_module
+        from furious import config
+        module = get_persistence_module('furious.config')
+        self.assertEqual(module, config)
 
     def test_get_config_empty_yaml(self):
         from furious.config import EmptyYamlFile
+
         os.path.exists = Mock(return_value=True)
         example_yaml = str('')
-        self.mock_get_config_raises(example_yaml,EmptyYamlFile)
+        self.mock_get_config_raises(example_yaml, EmptyYamlFile)
 
     def test_get_config_with_no_yaml_file(self):
         os.path.exists = Mock(return_value=False)
-        from ..config import get_config
-        config = get_config()
+        from ..config import _parse_yaml_config
 
-        self.assertEqual(config,{'secret_key':
-         '931b8-i-f44330b4a5-am-3b9b733f-not-secure-043e96882',
-                                 'persistence': 'ndb',
-                                 'task_system': 'appengine_taskqueue'})
+        config = _parse_yaml_config()
 
+        self.assertEqual(config, {'secret_key':
+                                  '931b8-i-f44330b4a5-am-3b9b733f-not-secure-043e96882',
+                                  'persistence': 'ndb',
+                                  'task_system': 'appengine_taskqueue'})
