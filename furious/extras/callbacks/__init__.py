146ad554dcb11651f7278d511c10438f370c998c
John.Lockwood <john.lockwood@webfilings.com>
2013-02-15 15:28:32 -0600

    Remove obsolete code from job callback

--- furious/extras/callbacks/__init__.py
+++ furious/extras/callbacks/__init__.py
@@ -16,11 +16,9 @@
 
 #collection of context callbacks for different occasions
 import logging
-from google.appengine.api import memcache
-import time
 
 
-def small_aggregated_results_success_callback(idx,result):
+def small_aggregated_results_success_callback(idx, result):
     """
     args:
         id: the job id
@@ -31,15 +29,4 @@ def small_aggregated_results_success_callback(idx,result):
     In this case it checks the real time the job took
     At this point, the result can be no larger then 1MB
     """
-    start_time = memcache.get("JOBTIMESTART:{0}".format(idx))
-    job_time = 0
-    if start_time:
-        try:
-            start_time_f = float(start_time)
-            job_time = time.time() - start_time_f
-            logging.info("Job {0} took {1}".format(idx, job_time))
-        except ValueError:
-            pass
-
-    #Flag the job as done
-    memcache.set("Furious:{0}".format(idx), "done by callback")
+    logging.info("job done id: {0}".format(idx))
