57085b693c9d6e72a626c9559f71954fcf80bbae
John.Lockwood <john.lockwood@webfilings.com>
2013-02-20 22:38:42 -0600

    Fix comment formatting and relative imports

--- furious/tests/test_config.py
+++ furious/tests/test_config.py
@@ -20,18 +20,15 @@ from mock import patch
 
 class TestConfigurationLoading(unittest.TestCase):
     def test_load_yaml_config(self):
-        """Ensure _load_yaml_config will load a specified path
-
-        """
+        """Ensure _load_yaml_config will load a specified path."""
         from furious.config import _load_yaml_config
-        contents = _load_yaml_config(os.path.join('furious','_furious.yaml'))
+        contents = _load_yaml_config(os.path.join('furious', '_furious.yaml'))
         self.assertEqual(contents, "persistence: ndb\n")
 
 
     def test_module_import_missing_module(self):
         """Ensure module_import raises an exception when the specified module
         does not exist.
-
         """
         from furious.config import BadModulePathError
         from furious.config import module_import
@@ -40,20 +37,16 @@ class TestConfigurationLoading(unittest.TestCase):
                           module_import, 'furious.extras.not_here')
 
     def test_module_import(self):
-        """Ensure module_import returns a reference to the expected module.
-
-        """
+        """Ensure module_import returns a reference to the expected module."""
         from furious.config import module_import
-        from .. import config
+        from furious import config
 
         module = module_import('furious.config')
         self.assertEqual(module, config)
-    
+
     @patch('os.path.exists', autospec=True)
     def test_not_find_yaml(self, mock_exists):
-        """Ensure when no furious.yaml exists, no file is found.
-
-        """
+        """Ensure when no furious.yaml exists, no file is found."""
         mock_exists.return_value = False
         from furious.config import find_furious_yaml
 
@@ -61,9 +54,7 @@ class TestConfigurationLoading(unittest.TestCase):
         self.assertIsNone(config_yaml_path)
 
     def test_get_config(self):
-        """Ensure a config contents produces the expected dictionary.
-
-        """
+        """Ensure a config contents produces the expected dictionary."""
         from furious.config import _parse_yaml_config
 
         example_yaml = str('secret_key: "blah"\n'
@@ -77,9 +68,7 @@ class TestConfigurationLoading(unittest.TestCase):
                                      'task_system': 'flah'})
 
     def test_get_configured_persistence_exists(self):
-        """Ensure a chosen persistence module is selected.
-
-        """
+        """Ensure a chosen persistence module is selected."""
         from furious.config import _parse_yaml_config
 
         example_yaml = str('secret_key: "blah"\n'
@@ -88,7 +77,7 @@ class TestConfigurationLoading(unittest.TestCase):
 
         my_config = _parse_yaml_config(example_yaml)
 
-        from .. import config
+        from furious import config
 
         config._config = my_config
 
@@ -98,9 +87,7 @@ class TestConfigurationLoading(unittest.TestCase):
         self.assertEqual(persistence_module, config)
 
     def test_get_config_invalid_yaml(self):
-        """Ensure an invalid yaml file will raise InvalidYamlFile.
-
-        """
+        """Ensure an invalid yaml file will raise InvalidYamlFile."""
         from furious.config import InvalidYamlFile
         from furious.config import _parse_yaml_config
 
@@ -111,18 +98,14 @@ class TestConfigurationLoading(unittest.TestCase):
         self.assertRaises(InvalidYamlFile, _parse_yaml_config, example_yaml)
 
     def test_get_persistence_module(self):
-        """Ensure the chosen persistence module will load a module.
-
-        """
+        """Ensure the chosen persistence module will load a module."""
         from furious.config import get_persistence_module
         from furious import config
         module = get_persistence_module('furious.config')
         self.assertEqual(module, config)
 
     def test_get_config_empty_yaml(self):
-        """Ensure an empty furious.yaml will produce a default config.
-
-        """
+        """Ensure an empty furious.yaml will produce a default config."""
         from furious.config import default_config
         from furious.config import _parse_yaml_config
         example_yaml = str('')
