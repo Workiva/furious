d52df2ed199044d7a97fb52c08604cc99bae33e9
Eric Olson <eric.olson@webfilings.com>
2013-02-25 14:38:24 -0600

    Ensure tasks don't pollute os.environ
    If tasks have a modified os.environ, raise an exception.
    This is to help prevent tasks from interfering with each other.

--- furious/test_stubs/appengine/queues.py
+++ furious/test_stubs/appengine/queues.py
@@ -7,6 +7,7 @@ Advanced app engine features such as automatic retries are not implemented.
 """
 
 import base64
+import copy
 import os
 import uuid
 
@@ -14,22 +15,46 @@ from furious.context._local import _clear_context
 from furious.handlers import process_async_task
 
 
+class EnvModifiedException(Exception):
+    """Indicates an environment has been modified and not returned to its
+    original state.
+    """
+
+
 def _execute_task(task):
-    """Extract the body and header in the task and execute the task."""
+    """Extract the body and header from the task and process it.
+
+    Raise an exception if the task leaves modifications to the environment.
+    """
 
     # Ensure each test looks like it is in a new request.
     os.environ['REQUEST_ID_HASH'] = uuid.uuid4().hex
 
+    # Make a copy of the environment so we can ensure it's not changed later.
+    orig_environ = copy.deepcopy(os.environ)
+
+    # Decode the body and process the task.
     body = base64.b64decode(task['body'])
     return_code, func_path = process_async_task(task['headers'], body)
 
-    # TODO: Possibly do more with return_codes
+    # TODO: Possibly do more with return_codes.
+
+    # Ensure users have not modified the execution environment.
+    modified_environment = False
+
+    if not (os.environ == orig_environ):
+        modified_environment = True
+        env_error_msg = ("Environment was modified by task and not restored\n"
+                         + "orig env: %s\n\n, current env: %s" %
+                         (orig_environ, os.environ))
 
     # Cleanup context since we will be executing more tasks in this process.
     _clear_context()
-
-    # cleanup
     del os.environ['REQUEST_ID_HASH']
+
+    # Now that our environment changes are cleaned up, raise any exceptions.
+    if modified_environment:
+        raise EnvModifiedException(env_error_msg)
 
 
 def _execute_queue(queue_name, queue_service):
