6b3fffd561dcb6aeee12b2a81b09ab9b1089a04f
Ross Hendrickson <ross.hendrickson@webfilings.com>
2014-03-06 00:56:11 -0600

    Add doc strings. Add get current Async to handle

--- furious/complete.py
+++ furious/complete.py
@@ -18,7 +18,10 @@ from . import errors
 
 
 def initialize_completion(node):
-    """
+    """ Will create the initial completion graph with Kepler and then
+    add any children required to it.
+
+    :param node: :class: `Async` or `Context`
     """
 
     work_ids = [node.id]
@@ -43,26 +46,37 @@ def initialize_completion(node):
 
 
 def add_context_work(completion_id, parent, asyncs, args=None):
+    """Handles adding more work to the graph underneath an Async or a Context.
+
+    :param completion_id: :class: `str`
+    :param parent: :class: `Async` or `Context`
+    :param asyncs: :class: `list` of `Async`
+    :param args: : :class: `dict`
+
+    """
 
     if len(asyncs) < 1:
         return
 
     work_ids = []
+    if not args:
+        args = _gen_callback_args(parent)
 
     for async in asyncs:
         async.completion_id = completion_id
         async.process_results = _process_completion_result
         work_ids.append(async.id)
 
-    if not args:
-        args = _gen_callback_args(parent)
-
+    # The completion_callback will handle decoding the actual Async or Context
+    # to run after the work is finished.
     add_work_to_work_id(completion_id, parent.id, work_ids,
                         on_success=completion_callback,
                         on_success_args=args)
 
 
 def _gen_callback_args(node):
+    """Helper to extract the callback dict from the options dict.
+    """
 
     callbacks = node._options.get('callbacks')
     args = {}
@@ -73,7 +87,11 @@ def _gen_callback_args(node):
 
 
 def handle_completion_start(node):
+    """ This will ensure that if there is any competion graph work to be done
+    when an Async or a Context is 'started' that the work will be done before
+    we insert any tasks.
 
+    """
     callbacks = node._options.get('callbacks')
 
     if not callbacks:
@@ -82,20 +100,26 @@ def handle_completion_start(node):
     if not node.on_success and not node.on_failure:
         return
 
-    # If we are in a context with a completion id then we need to add to it
+    # If we are in a Context or an Async with a completion id
+    # then we need to add to that completion graph
     current_context = None
     try:
         current_context = get_current_context()
+
+        if not current_context:
+            current_context = get_current_async()
+
     except errors.NotInContextError:
         logging.debug('no context')
 
     # not in a context or an async
-    # no completion graph
+    # need to build a new completion graph
     if not current_context or not current_context.completion_id:
         initialize_completion(node)
         return
 
-    # we assume that we are in a context or part of an async
+    # in a context or part of an async
+    # add to the existing completion graph
     if isinstance(node, Context):
         add_context_work(current_context.completion_id, node, node._tasks)
     else:
@@ -104,22 +128,26 @@ def handle_completion_start(node):
 
 
 def _process_completion_result():
+    """When the async is finished being run it will call a process results
+    function that has been set in the options. This will do the work of
+    marking the Async complete in the completion graph.
+
+    """
     from furious.processors import AsyncException
 
     async = get_current_async()
 
     if isinstance(async.result, AsyncException):
-        mark_async_complete(async, True, {'exception': async.result})
+        payload = {'exception': async.result}
+        mark_work_complete(async.completion_id, async.id, True, payload)
 
-    mark_async_complete(async)
-
-
-def mark_async_complete(async, failed=False, failed_payload=None):
-
-    mark_work_complete(async.completion_id, async.id, failed, failed_payload)
+    mark_work_complete(async.completion_id, async.id)
 
 
 def execute_completion_callbacks(callbacks, failed=False, exception=None):
+    """Decodes the on_success and on_failure callbacks which could be
+    Asyncs or Contexts and then executes them.
+    """
 
     if not callbacks:
         return
