fd46372e7c8b6d0a181b9be0af1d812351874ec4
Robert Kluin <robert.kluin@webfilings.com>
2014-05-07 19:10:42 -0600

    Add simple ndb_persitence Context storage model

--- furious/extras/appengine/ndb_persistence.py
+++ furious/extras/appengine/ndb_persistence.py
@@ -1,3 +1,50 @@
+#
+# Copyright 2014 WebFilings, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+"""This module contains the default functions to use when performing
+persistence operations backed by the App Engine ndb library.
+"""
+
+
+from google.appengine.ext import ndb
+
+
+class FuriousContextNotFoundError(Exception):
+    """FuriousContext entity not found in the datastore."""
+
+
+class FuriousContext(ndb.Model):
+    context = ndb.JsonProperty(indexed=False, compressed=True)
+
+    @classmethod
+    def from_context(cls, context):
+        """Create a `cls` entity from a context."""
+        return cls(id=context.id, context=context.to_dict())
+
+    @classmethod
+    def from_id(cls, id):
+        """Load a `cls` entity and instantiate the Context it stores."""
+        from furious.context import Context
+
+        # TODO: Handle exceptions and retries here.
+        entity = cls.get_by_id(id)
+        if not entity:
+            raise FuriousContextNotFoundError(
+                "Context entity not found for: {}".format(id))
+
+        return Context.from_dict(entity.context)
 
 def store_async_result(async):
     """Persist the Async's result to the datastore."""
